0c88498128c0a82de4651e3547c2101e3e85e85b
JimmyTsai <qaz3182@gmail.com>
2020-01-09 23:10:41 +0800

    add rspec 120 min

--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -46,5 +46,12 @@ RSpec.describe Parking, type: :model do
       parking.calculate_amount
       expect( parking.amount ).to eq(300)
     end
+
+    it "120 mins should be ¥4" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest", :start_at => t, :end_at => t + 120.minutes )
+      parking.calculate_amount
+      expect( parking.amount ).to eq(400)
+    end
   end
 end
