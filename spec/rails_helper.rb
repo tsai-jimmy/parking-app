43edc51c7e903aa8eb2feb8e7038c3fec621e61b
JimmyTsai <qaz3182@gmail.com>
2020-01-10 16:26:35 +0800

    fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-# require 'rspec/rails'
-# require 'capybara/rails'
-require 'capybara/rspec'
-
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -11,6 +7,8 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
