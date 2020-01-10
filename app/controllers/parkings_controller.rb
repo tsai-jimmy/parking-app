bbfe69c81ee9002a15c6f59ac2d68e5c545ee59a
JimmyTsai <qaz3182@gmail.com>
2020-01-10 16:16:48 +0800

    refactor before validation setup amount

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -31,8 +31,6 @@ class ParkingsController < ApplicationController
   def update
     @parking = Parking.find(params[:id])
     @parking.end_at = Time.now
-    @parking.calculate_amount
-
     @parking.save!
 
     redirect_to parking_path(@parking)
--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Parking, type: :model do
   describe ".validate_end_at_with_amount" do
-
-    it "is invalid without amount" do
-      parking = Parking.new( :parking_type => "guest",
-                             :start_at => Time.now - 6.hours,
-                             :end_at => Time.now)
-      expect( parking ).to_not be_valid
-    end
-
     it "is invalid without end_at" do
       parking = Parking.new( :parking_type => "guest",
                              :start_at => Time.now - 6.hours,
@@ -33,31 +25,31 @@ RSpec.describe Parking, type: :model do
 
       it "30 mins should be ¥2" do
         @parking.end_at = @time + 30.minutes
-        @parking.calculate_amount
+        @parking.save
         expect(@parking.amount).to eq(200)
       end
 
       it "60 mins should be ¥2" do
         @parking.end_at = @time + 60.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(200)
       end
 
       it "61 mins should be ¥3" do
         @parking.end_at = @time + 61.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(300)
       end
 
       it "90 mins should be ¥3" do
         @parking.end_at = @time + 90.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(300)
       end
 
       it "120 mins should be ¥4" do
         @parking.end_at = @time + 120.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(400)
       end
     end
@@ -71,31 +63,31 @@ RSpec.describe Parking, type: :model do
 
       it "30 mins should be ¥2" do
         @parking.end_at = @time + 30.minutes
-        @parking.calculate_amount
+        @parking.save
         expect(@parking.amount).to eq(200)
       end
 
       it "60 mins should be ¥2" do
         @parking.end_at = @time + 60.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(200)
       end
 
       it "61 mins should be ¥2.5" do
         @parking.end_at = @time + 61.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(250)
       end
 
       it "90 mins should be ¥2.5" do
         @parking.end_at = @time + 90.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(250)
       end
 
       it "120 mins should be ¥3" do
         @parking.end_at = @time + 120.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(300)
       end
     end
@@ -108,43 +100,43 @@ RSpec.describe Parking, type: :model do
 
       it "30 mins should be ¥12" do
         @parking.end_at = @time + 30.minutes
-        @parking.calculate_amount
+        @parking.save
         expect(@parking.amount).to eq(1200)
       end
 
       it "60 mins should be ¥12" do
         @parking.end_at = @time + 60.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(1200)
       end
 
       it "61 mins should be ¥12" do
         @parking.end_at = @time + 61.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(1200)
       end
 
       it "90 mins should be ¥12" do
         @parking.end_at = @time + 90.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(1200)
       end
 
       it "120 mins should be ¥12" do
         @parking.end_at = @time + 120.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(1200)
       end
 
       it "360 mins should be ¥12" do
         @parking.end_at = @time + 360.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(1200)
       end
 
       it "361 mins should be ¥12" do
         @parking.end_at = @time + 361.minutes
-        @parking.calculate_amount
+        @parking.save
         expect( @parking.amount ).to eq(1600)
       end
     end
